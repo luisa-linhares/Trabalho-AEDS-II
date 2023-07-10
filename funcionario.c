09038a26ae1df1f4786e4c8f9c39ba3a184d7bf9
Luisa Linhares <luisalaura39@gmail.com>
2023-07-10 20:04:31 -0300

    Implementações da aula 8
    Implementações da aula 8

--- funcionario.c
+++ funcionario.c
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-=======
-//
-// Created by Vanessa Braganholo on 16/09/2018.
-//
-
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 #include "funcionario.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
-<<<<<<< HEAD
 #include <time.h>
-=======
-#include<time.h>
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
+#include <math.h>
+#include "pilha.h"
 
 // Imprime funcionario
 void imprime(TFunc *func) {
     printf("**********************************************");
-<<<<<<< HEAD
     printf("\nFuncionario de codigo ");
-=======
-    printf("\nFuncionario de código ");
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
     printf("%d", func->cod);
     printf("\nNome: ");
     printf("%s", func->nome);
@@ -30,20 +17,12 @@ void imprime(TFunc *func) {
     printf("%s", func->cpf);
     printf("\nData de Nascimento: ");
     printf("%s", func->data_nascimento);
-<<<<<<< HEAD
     printf("\nSalario: ");
-=======
-    printf("\nSalário: ");
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
     printf("%4.2f", func->salario);
     printf("\n**********************************************");
 }
 
-<<<<<<< HEAD
 // Fun��o auxiliar para gerar uma string aleat�ria
-=======
-// Função auxiliar para gerar uma string aleatória
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 void gera_string_aleatoria(char* str, int tamanho) {
     static const char caracteres[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     int i;
@@ -53,20 +32,12 @@ void gera_string_aleatoria(char* str, int tamanho) {
     str[tamanho - 1] = '\0';
 }
 
-<<<<<<< HEAD
 // Fun��o auxiliar para gerar um CPF aleat�rio
-=======
-// Função auxiliar para gerar um CPF aleatório
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 void gera_cpf_aleatorio(char* cpf) {
     sprintf(cpf, "%03d.%03d.%03d-%02d", rand() % 1000, rand() % 1000, rand() % 1000, rand() % 100);
 }
 
-<<<<<<< HEAD
 // Gera um funcion�rio aleat�rio
-=======
-// Gera um funcionário aleatório
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 TFunc* funcionario_aleatorio(int cod) {
     TFunc* func = (TFunc*)malloc(sizeof(TFunc));
 
@@ -81,11 +52,7 @@ TFunc* funcionario_aleatorio(int cod) {
 // Salva funcionario no arquivo out, na posicao atual do cursor
 void salva(TFunc *func, FILE *out) {
     fwrite(&func->cod, sizeof(int), 1, out);
-<<<<<<< HEAD
     //func->nome ao inv�s de &func->nome, pois string j� � ponteiro
-=======
-    //func->nome ao invés de &func->nome, pois string já é ponteiro
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
     fwrite(func->nome, sizeof(char), sizeof(func->nome), out);
     fwrite(func->cpf, sizeof(char), sizeof(func->cpf), out);
     fwrite(func->data_nascimento, sizeof(char), sizeof(func->data_nascimento), out);
@@ -107,7 +74,6 @@ TFunc *le(FILE *in) {
     return func;
 }
 
-<<<<<<< HEAD
 TFunc* busca_sequencial_por_codigo(FILE* in, int codigo) {
     TFunc* func;
     rewind(in); // Retorna ao in�cio do arquivo
@@ -120,71 +86,28 @@ TFunc* busca_sequencial_por_codigo(FILE* in, int codigo) {
 
         comparacoes++; // Incrementa o contador de compara��es
 
-=======
-TFunc* busca_por_codigo(FILE* in, int codigo) {
-    TFunc* func;
-
-    rewind(in); // Retorna ao início do arquivo
-
-    while ((func = le(in)) != NULL) {
-        if (func->cod == codigo) {
-            return func; // Retorna o funcionário encontrado
-        }
-        free(func);
-    }
-
-    return NULL; // Funcionário não encontrado
-}
-
-TFunc* busca_sequencial_por_codigo(FILE* in, int codigo) {
-    TFunc* func;
-    rewind(in); // Retorna ao início do arquivo
-
-    clock_t start_time = clock(); // Inicia a contagem de tempo
-
-     int comparacoes = 0; // Variável para contar o número de comparações
-
-    while ((func = le(in)) != NULL) {
-
-        comparacoes++; // Incrementa o contador de comparações
-      
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
         if (func->cod == codigo) {
             clock_t end_time = clock(); // Finaliza a contagem de tempo
             double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
             printf("\nTempo de busca: %.4f segundos\n", elapsed_time);
 
-<<<<<<< HEAD
             printf("\nNumero de comparacoes: %d\n", comparacoes);
 
             return func; // Retorna o funcion�rio encontrado
         }
 
-=======
-            printf("\nNumero de comparações: %d\n", comparacoes);
-          
-            return func; // Retorna o funcionário encontrado
-        }
-      
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
         free(func);
     }
 
     clock_t end_time = clock(); // Finaliza a contagem de tempo
     double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
     printf("\nTempo de busca: %.4f segundos\n", elapsed_time);
-<<<<<<< HEAD
     printf("\nNumero de comparacoes: %d\n", comparacoes);
     return NULL; // Funcion�rio n�o encontrado
-=======
-    printf("\nNumero de comparações: %d\n", comparacoes);
-    return NULL; // Funcionário não encontrado
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 }
 
 TFunc*  busca_binaria_por_codigo(FILE* in, int codigo) {
     fseek(in, 0, SEEK_END); // Move o cursor para o final do arquivo
-<<<<<<< HEAD
     long size = ftell(in); // Obt�m o tamanho total do arquivo em bytes
 
     int num_regs = size / tamanho(); // Calcula o n�mero de registros no arquivo
@@ -207,51 +130,18 @@ TFunc*  busca_binaria_por_codigo(FILE* in, int codigo) {
 
         if (func == NULL) {
             break; // Sa� do loop se ocorrer um erro de leitura
-=======
-    long size = ftell(in); // Obtém o tamanho total do arquivo em bytes
-
-    int num_regs = size / tamanho(); // Calcula o número de registros no arquivo
-
-    int left = 0; // Índice inicial da busca
-    int right = num_regs - 1; // Índice final da busca
-
-    clock_t start_time = clock(); // Inicia a contagem de tempo
-  
-    int comparacoes = 0; // Variável para contar o número de comparações
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2; // Calcula o índice do elemento do meio
-
-        fseek(in, mid * tamanho(), SEEK_SET); // Move o cursor para a posição do meio
-
-        TFunc* func = le(in); // Lê o registro na posição atual
-
-        comparacoes++; // Incrementa o contador de comparações
-
-        if (func == NULL) {
-            break; // Saí do loop se ocorrer um erro de leitura
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
         }
 
         if (func->cod == codigo) {
            clock_t end_time = clock(); // Finaliza a contagem de tempo
            double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
            printf("\nTempo de busca: %.4f segundos\n", elapsed_time);
-<<<<<<< HEAD
            printf("\nNumero de comparacoes: %d\n", comparacoes);
            return func; // Retorna o funcion�rio encontrado
         } else if (func->cod < codigo) {
             left = mid + 1; // Atualiza o �ndice inicial para a busca na metade superior
         } else {
             right = mid - 1; // Atualiza o �ndice final para a busca na metade inferior
-=======
-           printf("\nNumero de comparações: %d\n", comparacoes);
-           return func; // Retorna o funcionário encontrado
-        } else if (func->cod < codigo) {
-            left = mid + 1; // Atualiza o índice inicial para a busca na metade superior
-        } else {
-            right = mid - 1; // Atualiza o índice final para a busca na metade inferior
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
         }
 
         free(func);
@@ -261,7 +151,6 @@ TFunc*  busca_binaria_por_codigo(FILE* in, int codigo) {
     double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
    printf("\nTempo de busca: %.4f segundos\n", elapsed_time);
 
-<<<<<<< HEAD
   printf("\nNumero de comparacoes: %d\n", comparacoes);
 
     return NULL; // Funcion�rio n�o encontrado
@@ -296,11 +185,6 @@ void insertion_sort(FILE *arq, int tam) {
     //descarrega o buffer para ter certeza que dados foram gravados
     fflush(arq);
 
-=======
-  printf("\nNumero de comparações: %d\n", comparacoes);
-
-    return NULL; // Funcionário não encontrado
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 }
 
 // Retorna tamanho do funcionario em bytes
@@ -312,14 +196,209 @@ int tamanho() {
            + sizeof(double); //salario
 }
 
-<<<<<<< HEAD
 int qtdRegistros(FILE *in){
     fseek(in, 0, SEEK_END);
     int tam = trunc(ftell(in))/tamanho();
     return tam;
 }
 
-=======
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
+void classificacaoInterna(FILE *arq, int M) {
+
+    rewind(arq); //posiciona cursor no inicio do arquivo
+
+
+    int reg = 0;
+    int nFunc = qtdRegistros(arq);
+    int qtdParticoes = 0;
+    int t = tamanho();
+    char nome[10];
+    char numero[5];
+    char extensao[5];
+
+        //itoa(qtdParticoes,numero,10);
+
+    while (reg != nFunc) {
+        //le o arquivo e coloca no vetor
+        TFunc *v[M];
+        int i = 0;
+        while (!feof(arq)) {
+            fseek(arq, (reg) * tamanho(), SEEK_SET);
+            v[i] = le(arq);
+       //     imprime_funcionario(v[i]);
+            i++;
+            reg++;
+            if(i>=M) break;
+        }
+
+        //ajusta tamanho M caso arquivo de entrada tenha terminado antes do vetor
+        if (i != M) {
+            M = i;
+        }
+
+        //faz ordenacao
+        for (int j = 1; j < M; j++) {
+            TFunc *f = v[j];
+            i = j - 1;
+            while ((i >= 0) && (v[i]->cod > f->cod)) {
+                v[i + 1] = v[i];
+                i = i - 1;
+            }
+            v[i + 1] = f;
+        }
+
+        //cria arquivo de particao e faz gravacao
+        printf("\n\nParticao %d",qtdParticoes);
+        strcpy(nome,"particao");
+        strcpy(extensao,".dat");
+        itoa(qtdParticoes,numero,10);
+        strcat(nome,numero);
+        strcat(nome,extensao);
+
+        //printf("\n%s\n", nome);
+
+        FILE *p;
+
+        if ((p = fopen(nome, "wb+")) == NULL) {
+            printf("Erro criar arquivo de saida\n");
+        } else {
+            for (int i = 0; i < M; i++) {
+                fseek(p, (i) * tamanho(), SEEK_SET);
+                salva(v[i], p);
+                imprime(v[i]);
+            }
+            fclose(p);
+            qtdParticoes++;
+        }
+        for(int jj = 0; jj<M; jj++)
+            free(v[jj]);
+    }
+    //nParticoes = qtdParticoes;
+}
+
+
+
+void intercalacao_basico(FILE *out, int num_p) {
+
+    char nome[10];
+    char numero[5];
+    char extensao[5];
+
+                    //criando pilha com o conteudo das particoes geradas
+                    TPilha **vetPilhas;
+                    vetPilhas = (TPilha **) malloc(sizeof(TPilha *) * (num_p));
+                    int *vetTopo;
+                    vetTopo = (int *) malloc(sizeof(int) * (num_p));
+
+                    for(int p = 0 ; p < num_p ; p++){
+                        //alocando a memória para salvar uma pilha para cada particao
+                        vetPilhas[p] = (TPilha *) malloc(sizeof(TPilha) * (8));
+                        //abrindo primeira partição para leitura
+                        strcpy(nome,"particao");
+                        strcpy(extensao,".dat");
+                        itoa(p,numero,10);
+                        strcat(nome,numero);
+                        strcat(nome,extensao);
+                        FILE *part = fopen(nome, "rb+");
+                        //printf("%s \n", "Particao 0");
+                        //nomes = nomes->prox;
+                        //imprimindo os funcionários gravados nas partições
+                        //imprimirBase(part);
+                        //inicializando os topos das pilhas
+                        vetTopo[p] = -1;
+                        //criando pilha através da leitura da partição
+                        cria_pilha_particao(vetPilhas[p], part, 8, &vetTopo[p]);
+                        //fechando a partição
+                        fclose(part);
+                    }
+                    //crioupilhas = 1;
+                    //nomes = prox;
+
+    int fim = 0; //variavel que controla fim do procedimento
+
+
+
+        //cria vetor de particoes
+        TVet v[num_p];
+
+        //abre arquivos das particoes, colocando variavel de arquivo no campo f do vetor
+        //e primeiro funcionario do arquivo no campo func do vetor
+        for (int i=0; i < num_p; i++) {
+            strcpy(nome,"particao");
+            strcpy(extensao,".dat");
+            itoa(i,numero,10);
+            strcat(nome,numero);
+            strcat(nome,extensao);
+
+            //printf("%s",nome);
+
+            v[i].f = fopen(nome, "rb");
+            v[i].aux_p = 0;
+
+            if (v[i].f != NULL) {
+                fseek(v[i].f, v[i].aux_p * tamanho(), SEEK_SET);
+                TFunc *f = le(v[i].f);
+                if (f == NULL) {
+                    //arquivo estava vazio
+                    //coloca HIGH VALUE nessa posi??o do vetor
+                    v[i].func = funcionario_aleatorio(INT_MAX);
+                }
+                else {
+                    //conseguiu ler funcionario, coloca na posi??o atual do vetor
+                    v[i].func = f;
+                }
+            }
+            else {
+                fim = 1;
+            }
+            //nome_particoes = nome_particoes->prox;
+        }
+
+        int aux = 0;
+        while (!(fim)) { //conseguiu abrir todos os arquivos
+            int menor = INT_MAX;
+            int pos_menor;
+            //encontra o funcionario com menor chave no vetor
+            for(int i = 0; i < num_p; i++){
+                if(v[i].func->cod < menor){
+                    menor = v[i].func->cod;
+                    pos_menor = i;
+                }
+            }
+            if (menor == INT_MAX) {
+                fim = 1; //terminou processamento
+            }
+            else {
+                //salva funcionario no arquivo de saída
+                fseek(out, aux * tamanho(), SEEK_SET);
+                salva(v[pos_menor].func, out);
+                //printf("%d ",pos_menor);
+                //atualiza posição pos_menor do vetor com pr?ximo funcionario do arquivo
+                v[pos_menor].aux_p++;
+                fseek(v[pos_menor].f, v[pos_menor].aux_p * tamanho(), SEEK_SET);
+                TFunc *f = le(v[pos_menor].f);
+                aux++;
+                if (f == NULL) {
+                    //arquivo estava vazio
+                    //coloca HIGH VALUE nessa posiçao do vetor
+                    v[pos_menor].func = funcionario_aleatorio(INT_MAX);
+                }
+                else {
+                    v[pos_menor].func = f;
+                }
+
+            }
+        }
+
+        //fecha arquivos das partiÇões de entrada
+        for(int i = 0; i < num_p; i++){
+            fclose(v[i].f);
+        //    free(v[i].func);
+        }
+        //fecha arquivo de saída
+        //fclose(out);
+
+}
+
+
 
 
--- funcionario.h
+++ funcionario.h
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 #ifndef FUNCIONARIO_H_INCLUDED
 #define FUNCIONARIO_H_INCLUDED
-=======
-//
-// Created by Vanessa Braganholo on 16/09/2018.
-//
-
-#ifndef FUNCIONARIOS_FUNCIONARIO_H
-#define FUNCIONARIOS_FUNCIONARIO_H
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -21,10 +12,17 @@ typedef struct Funcionario {
     double salario;
 } TFunc;
 
+//estrutura para armazenar o vetor de particoes
+typedef struct vetor{
+    TFunc *func;
+    int aux_p;
+    FILE *f;
+}TVet;
+
+
 // Imprime funcionario
 void imprime(TFunc *func);
 
-<<<<<<< HEAD
 // Fun��o auxiliar para gerar uma string aleat�ria
 void gera_string_aleatoria(char* str, int tamanho);
 
@@ -32,15 +30,6 @@ void gera_string_aleatoria(char* str, int tamanho);
 void gera_cpf_aleatorio(char* cpf);
 
 // Gera um funcion�rio aleat�rio
-=======
-// Função auxiliar para gerar uma string aleatória
-void gera_string_aleatoria(char* str, int tamanho);
-
-// Função auxiliar para gerar um CPF aleatório
-void gera_cpf_aleatorio(char* cpf);
-
-// Gera um funcionário aleatório
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 TFunc* funcionario_aleatorio(int cod);
 
 // Salva funcionario no arquivo out, na posicao atual do cursor
@@ -50,16 +39,10 @@ void salva(TFunc *func, FILE *out);
 // Retorna um ponteiro para funcionario lido do arquivo
 TFunc *le(FILE *in);
 
-<<<<<<< HEAD
-=======
-TFunc* busca_por_codigo(FILE* in, int codigo);
-
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
 TFunc* busca_sequencial_por_codigo(FILE* in, int codigo);
 
 TFunc* busca_binaria_por_codigo(FILE* in, int codigo);
 
-<<<<<<< HEAD
 void insertion_sort(FILE *arq, int tam);
 
 // Retorna tamanho do funcionario em bytes
@@ -68,10 +51,3 @@ int qtdRegistros(FILE *in);
 
 
 #endif // FUNCIONARIO_H_INCLUDED
-=======
-// Retorna tamanho do funcionario em bytes
-int tamanho();
-
-#endif //FUNCIONARIOS_FUNCIONARIO_H
-
->>>>>>> f17826373e93bee94fb1344d600d350d93b13972
